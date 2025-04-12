336f93928f18f623aa3858fc6a17a6b92786ac44
Deniz Egemen Emare <degemenemr0146@gmail.com>
2025-04-13 00:55:38 +0200

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // Netlify için optimize etme
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  },
+  output: 'export', // Static export for Netlify
+  distDir: 'out', // Output directory for static files
+};
+
+export default nextConfig;
